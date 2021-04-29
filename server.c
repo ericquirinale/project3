0733d322e2f1423dec7c7836ea5b1ef10f8e70e0
ericq12 <zbruins33@gmail.com>
2021-04-29 16:56:49 -0400

    Starting

---
+++ server.c
@@ -0,0 +1,15 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <signal.h>
+#include <pthread.h>
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
